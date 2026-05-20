edf67da71fe007c3b7d629c91625a60563527b2d
CapTightpants <88792220+CapTightpants@users.noreply.github.com>
2026-05-20 08:25:43 -0400

    Fixed setup.sh
    I actually teted the script this time

--- setup.sh
+++ setup.sh
@@ -1,7 +1,47 @@
 #!/bin/bash
 
+<<<<<<< Updated upstream
 printf 'Setting up venv...\n'
 python3 -m venv ./venv
 
 printf '\nDone!\nInstalling Dependencies...\n'
 ./venv/bin/pip3 install -r ./requirements.txt
+=======
+while [ 1 == 1 ]; do
+    printf '\nThis script will setup a virtual environment for SIFM and clone the configs to your printer_data. Continue? '
+    read -r -n 1 -p "Y/n: " answer
+
+    if [ -z "$answer" ]; then
+        answer="y"
+    fi
+
+    case "$answer" in
+        [Yy])
+            break
+            ;;
+        [Nn])
+            printf 'Goodbye.'
+            exit [N]
+            ;;
+        *)
+            printf '\nInvalid response.'
+            clear
+            ;;
+    esac
+done
+
+printf '\nSetting up venv...'
+python3 -m venv $HOME/SIFM/venv
+
+printf '\nDone!\nInstalling Dependencies...'
+./venv/bin/pip3 install -r $HOME/SIFM/requirements.txt
+
+printf '\nDone!\nCopying configs...\n'
+
+if [ -d $HOME/printer_data/config/ ]; then
+    cp -ir $HOME/SIFM/klipper/* $HOME/printer_data/config
+    printf '\nSuccess!\n'
+else
+    printf '\nprinter_data not found! Please copy the klipper configs manually.\n'
+fi
+>>>>>>> Stashed changes
